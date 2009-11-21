781595346bf9d794c27333653db9f31b298beb61
tranatuan24 <tranatuan24@gmail.com>
2009-11-21 12:08:58 +0000

    20091121 - Tuan - Create .java files in exceptions package

---
+++ src/exceptions/ColumnAlreadyExistsException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+/**
+ * 
+ */
+public class ColumnAlreadyExistsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/exceptions/NotLeafNodeException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+/** 
+ * 
+ */
+public class NotLeafNodeException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
