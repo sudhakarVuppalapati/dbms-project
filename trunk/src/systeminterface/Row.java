f1e1de6224c98a5ad55e51bc66f53b3c1fcb9bec
tranatuan24@gmail.com <tranatuan24@gmail.com@ae71a43a-cd35-11de-a502-cffa054868e3>
2009-11-21 12:14:01 +0000

    20091121 - Tuan - Create .java files in systeminterface package

---
+++ trunk/src/systeminterface/Row.java
@@ -0,0 +1,50 @@
+package systeminterface;
+
+import metadata.Type;
+import exceptions.NoSuchColumnException;
+
+/**
+ * Row represents horizontal representation for tuple
+ * 
+ */
+public interface Row {
+
+	/**
+	 * getColumnCount() return the number of columns in this row
+	 * 
+	 * @return Number of columns in the row
+	 */
+	public int getColumnCount();
+
+	/**
+	 * Get column type by column name
+	 * 
+	 * @param columnName
+	 *            Column name
+	 * @return Type of column
+	 * @throws NoSuchColumnException
+	 *             If column does not exist
+	 */
+	public Type getColumnType(String columnName) throws NoSuchColumnException;
+
+	/**
+	 * Get column type by column name
+	 * 
+	 * @return String[] name of all Columns in row
+	 * 
+	 */
+	public String[] getColumnNames();
+
+	/**
+	 * Get value stored in a the column at current row by column name
+	 * 
+	 * @param columnName
+	 *            Name of column
+	 * @return An object containing the value stored in that column (with the
+	 *         correct type)
+	 * @throws NoSuchColumnException
+	 *             If column does not exist
+	 */
+	public Object getColumnValue(String columnName)
+			throws NoSuchColumnException;
+}
