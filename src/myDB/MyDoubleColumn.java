6a151ab72884f99a4c79ff1c3784cf717b4732b9
razvan.belet <razvan.belet@ae71a43a-cd35-11de-a502-cffa054868e3>
2009-11-30 10:47:18 +0000

    20091130 - razvan - finished all column implementations

--- src/myDB/MyDoubleColumn.java
+++ src/myDB/MyDoubleColumn.java
@@ -6,18 +6,26 @@ import exceptions.NoSuchRowException;
 public class MyDoubleColumn extends MyColumn {
 	
 	private double[] data;
+	private byte[] statuses;
 	private int curSize;
 	
 	public MyDoubleColumn(String name, Type type){
 		super(name, type);
 		data= new double[this.defaulInitialCapacity];
+		statuses=new byte[this.defaulInitialCapacity];
 		curSize=0;
 	}
 	
 	public MyDoubleColumn(String name, Type type,int initialCapacity) {
 		super(name,type);
-		data = new double[initialCapacity];
+		data = new double[Math.round(initialCapacity*FACTOR)];
+		statuses=new byte[Math.round(initialCapacity*FACTOR)];
 		curSize=0;
+	}
+	
+	public MyDoubleColumn(String name,Type type,double[] data){
+		super(name,type);
+		this.data=data;
 	}
 	
 	@Override
@@ -37,14 +45,22 @@ public class MyDoubleColumn extends MyColumn {
 
 	@Override
 	public int getRowCount() {
-		return data.length;
+		return curSize;
 	}
 	
 	@Override
-	public void addData(Object newData){
+	public void add(Object newData){
+		//check if there is place for a new value
 		if(curSize == data.length){
-			System.arraycopy(data, 0, data1, 0, length)
+			//if not, allocate a new array
+			double[] data1=new double[Math.round(FACTOR*curSize)];
+			System.arraycopy(data, 0, data1, 0, curSize);
+			data=data1;
+			data=null; // try to force garbage collection
 		}
+		
+		//add the new value
+		data[curSize++]=((Double)newData).doubleValue();
 	}
 
 }
