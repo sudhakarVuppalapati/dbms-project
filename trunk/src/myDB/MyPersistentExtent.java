ba72ee40c3aaf178eac10f7ee1ef9ab9c5f2741d
tranatuan24@gmail.com <tranatuan24@gmail.com@ae71a43a-cd35-11de-a502-cffa054868e3>
2009-11-30 22:19:38 +0000

    20091130 - Tuan - MyPersistentExtent creation

--- trunk/src/myDB/MyPersistentExtent.java
+++ trunk/src/myDB/MyPersistentExtent.java
@@ -4,6 +4,7 @@
 package myDB;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -15,6 +16,12 @@ import systeminterface.PersistentExtent;
  */
 public class MyPersistentExtent implements PersistentExtent {
 
+	public static final String TABLES_METADATA_FILE = "disk/dbtables.dat";
+	
+	public static final int BUFF_SIZE = 1048576;
+	
+	public static final byte[] buff = new byte[BUFF_SIZE];
+	
 	@Override
 	public int appendData(byte[] data) throws IOException {
 		// TODO Auto-generated method stub
