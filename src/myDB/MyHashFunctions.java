06ab1caf0b21efd2b56d4a6f445baf70e9454da5
tranatuan24 <tranatuan24@gmail.com>
2009-12-23 20:04:53 +0000

    20091223 - Tuan - Create indirect index interface and code Extendible Hashing index. Still have some bugs though

---
+++ src/myDB/MyHashFunctions.java
@@ -0,0 +1,22 @@
+package myDB;
+
+public final class MyHashFunctions {
+	
+	/**
+	 * Based on Robert Jenkins' bit Mix Function, version in Java developed
+	 * by Thomas Wang (http://www.concentric.net/~Ttwang/tech/inthash.htm) 
+	 * Copyright (c) 2007 January 
+	 * @param key the input key, as 32-bit integer
+	 * @return the hash value
+	 */
+	public static int hash32shiftmult(int key)
+	{
+	  int c2=0x27d4eb2d; // a prime or an odd constant
+	  key = (key ^ 61) ^ (key >>> 16);
+	  key = key + (key << 3);
+	  key = key ^ (key >>> 4);
+	  key = key * c2;
+	  key = key ^ (key >>> 15);
+	  return key;
+	}
+}
